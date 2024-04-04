0b46c1e8e9a24c0928d5a914f742495119044fff
layanmoyura <layanmoyura@gmail.com>
2024-04-04 17:29:02 +0530

    hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,11 @@
-import Image from "next/image";
-import Link from "next/link";
+import Header from "./components/layout/Header";
+import Hero from "./components/layout/Hero";
 
 export default function Home() {
   return (
     <>
-      <header className="flex items-center justify-between">
-        <Link className="text-primary font-semibold text-2xl" href="/">
-          ST PIZZA
-        </Link>
-        <nav className="flex items-center gap-8 text-gray-500 font-semibold">
-          <Link href="">Home</Link>
-          <Link href="">Menu</Link>
-          <Link href="">About</Link>
-          <Link href="">Contact</Link>
-          <Link href="" className="bg-primary text-white px-6 py-2 rounded-full">Login</Link> 
-        </nav>
-      </header>
+      <Header />
+      <Hero />
     </>
   );
 }
