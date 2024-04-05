542b3728a985924500591690029f0917b8167f1b
layanmoyura <layanmoyura@gmail.com>
2024-04-05 23:47:36 +0530

    home menu mod 1

--- src/app/components/layout/HomeMenu.js
+++ src/app/components/layout/HomeMenu.js
@@ -1,9 +1,23 @@
+import Image from 'next/image'
+
 export default function HomeMenu() {
     return(
         <section className="text-center">
+            <div>
+                <div className="h-48 relative">
+                    <Image src="/sallad1.png" layout="fill" objectFit="contain" alt="sallad" />
+                </div>
+                <div className="h-48 relative">
+                    <Image src="/sallad2.png" layout="fill" objectFit="contain" alt="sallad" />
+                </div>
+            </div>
             <div className="text-center">
-            <h3>Check out</h3>
-            <h2>Menu</h2>
+                <h3 className="uppercase text-gray-600 font-semibold">
+                    Check out
+                </h3>
+                <h2 className="text-primary font-bold text-4xl italic">
+                    Menu
+                </h2>
             </div>
         </section>
     )
