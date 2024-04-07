ca24e87be9091728c919bb1e61dfe027900b437a
layanmoyura <layanmoyura@gmail.com>
2024-04-07 18:42:12 +0530

    landing page completed

--- src/app/components/layout/HomeMenu.js
+++ src/app/components/layout/HomeMenu.js
@@ -1,24 +1,32 @@
 import Image from 'next/image'
+import MenuItem from '../menu/MenuItem';
+import SectionHeader from './SectionHeader';
 
 export default function HomeMenu() {
-    return(
+    return (
         <section className="text-center">
-            <div>
-                <div className="h-48 relative">
-                    <Image src="/sallad1.png" layout="fill" objectFit="contain" alt="sallad" />
+            <div className="absolute left-0 right-0 w-full justify-start">
+                <div className="absolute left-0 -top-[70px] text-left -z-10">
+                    <Image src="/sallad1.png" width={109} height={189} alt="sallad" />
                 </div>
-                <div className="h-48 relative">
-                    <Image src="/sallad2.png" layout="fill" objectFit="contain" alt="sallad" />
+                <div className="absolute -top-[100px] right-0 -z-10">
+                    <Image src="/sallad2.png" width={107} height={195} objectFit="contain" alt="sallad" />
                 </div>
             </div>
-            <div className="text-center">
-                <h3 className="uppercase text-gray-600 font-semibold">
-                    Check out
-                </h3>
-                <h2 className="text-primary font-bold text-4xl italic">
-                    Menu
-                </h2>
+            <div className="text-center mb-4">
+                <SectionHeader
+                    subHeading="Check out"
+                    heading="Menu"
+                />
+            </div>
+            <div className="grid grid-cols-3 gap-4">
+                <MenuItem />
+                <MenuItem />
+                <MenuItem />
+                <MenuItem />
+                <MenuItem />
+                <MenuItem />
             </div>
         </section>
-    )
+    );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Header from "./components/layout/Header";
 import Hero from "./components/layout/Hero";
 import HomeMenu from "./components/layout/HomeMenu";
+import SectionHeader from "./components/layout/SectionHeader";
 
 export default function Home() {
   return (
@@ -8,6 +9,38 @@ export default function Home() {
       <Header />
       <Hero />
       <HomeMenu />
+      <section className="text-center my-16">
+        <SectionHeader 
+          subHeading="Our Story"
+          heading="About Us"
+      /> 
+      <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
+      <p>
+        We are a family-owned pizza restaurant that has been serving the community for over 30 years. Our secret recipe has been passed down from generation to generation, and we take pride in making the best pizza in town. Our mission is to provide our customers with delicious food and great service at an affordable price. We look forward to serving you!
+      </p>
+      <p>
+        we are a family-owned pizza restaurant that has been serving the community for over 30 years. Our secret recipe has been passed down from generation to generation, and we take pride in making the best pizza in town. Our mission is to provide our customers with delicious food and great service at an affordable price. We look forward to serving you!
+      </p>
+      
+      </div> 
+      
+      </section>
+      <section className="text-center my-16">
+        <SectionHeader 
+          subHeading="Don't be shy"
+          heading="Contact Us"
+        />
+        <div className="mt-8">
+          <a className="text-4xl underline text-gray-500" href="tel:+94761234567">
+            +94 76 123 4567
+          </a>
+        </div>
+      </section>
+      <footer>
+        <div className="border-t p-8 text-center text-gray-500 mt-16">
+          <p>&copy; 2024 Pizzeira. All rights reserved.</p>
+        </div>
+      </footer>
     </>
   );
 }
